c21b68f9a2d3279c520b5767e78bfc74d6dfd34e
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-02 09:51:29 +0530

    UC3:reverse pallimdrome

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@ class UseCase2PalindromeCheckerApp {
     public static void main(String[] args) {
         String str = "madam";
         String reversed = "";
-
         for (int i = str.length() - 1; i >= 0; i--) {
             reversed += str.charAt(i);
         }
-
         if (str.equals(reversed)) {
             System.out.println(str + " is a Palindrome");
         } else {
