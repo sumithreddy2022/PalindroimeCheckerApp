1ac669bc9157a4315369f3470dee943778cd7ee1
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-02 11:10:40 +0530

    UC8: Linked List Based Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,16 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
-class UseCase7PalindromeCheckerApp{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
-        Deque<Character> deque=new ArrayDeque<>();
-        for(char c:input.toCharArray()){
-            deque.addLast(c);
-        }
-        boolean isPalindrome=true;
-        while(deque.size()>1){
-            if(deque.removeFirst()!=deque.removeLast()){
-                isPalindrome=false;
-                break;
-            }
-        }
-        if(isPalindrome){
-            System.out.println("Palindrome");
-        }else{
-            System.out.println("Not Palindrome");
-        }
+import java.util.*;
+ class UseCase8PalindromeCheckerApp{
+    static class N{char d;N n;N(char d){this.d=d;}}
+    static N r(N h){N p=null;while(h!=null){N t=h.n;h.n=p;p=h;h=t;}return p;}
+    public static void main(String[] a){
+        Scanner s=new Scanner(System.in);
+        String x=s.nextLine();
+        N h=null,t=null;
+        for(char c:x.toCharArray()){N n=new N(c);if(h==null)h=t=n;else{t.n=n;t=n;}}
+        N f=h,sl=h;
+        while(f!=null&&f.n!=null){sl=sl.n;f=f.n.n;}
+        N sh=r(sl);
+        while(sh!=null&&h.d==sh.d){h=h.n;sh=sh.n;}
+        System.out.println(sh==null?"Palindrome":"Not Palindrome");
     }
 }
