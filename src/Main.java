9a4955ec7d1233b50897000170805cd7e9a97061
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-02 10:02:23 +0530

    UC4: Character Array Based Palindrome Check

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
-class UseCase2PalindromeCheckerApp {
+import java.util.Scanner;
+class UseCase4PalindromeCheckerApp {
     public static void main(String[] args) {
-        String str = "madam";
-        String reversed = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        boolean isPalindrome = true;
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
         }
-        if (str.equals(reversed)) {
-            System.out.println(str + " is a Palindrome");
+        if (isPalindrome) {
+            System.out.println("Palindrome");
         } else {
-            System.out.println(str + " is not a Palindrome");
+            System.out.println("Not a Palindrome");
         }
+        sc.close();
     }
 }
