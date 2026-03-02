14c25d6dc7daefdc1ae1d75912c6270ed5c3be69
sumithreddy2022 <sumithreddy202@gmail.com>
2026-03-02 09:35:54 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class UseCase2PalindromeCheckerApp {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String str = "madam";
+        String reversed = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a Palindrome");
+        } else {
+            System.out.println(str + " is not a Palindrome");
         }
     }
 }
